898c7f411929bb97557261b57d7cc14d52e5b0ed
Tim Tyrrell <timomitchel23@gmail.com>
2017-09-22 15:38:49 -0600

    iteration 2

--- test/card_test.rb
+++ test/card_test.rb
@@ -1,4 +1,6 @@
+require 'pry'
 require 'minitest/autorun'
+require "./lib/guess"
 require './lib/card'
 
 class CardTest < Minitest::Test
@@ -15,12 +17,21 @@ class CardTest < Minitest::Test
     assert_equal 'Juneau', card.answer
   end
 
-  def test_guess
+  def test_guess_returns_correct_feedback_and_boolean_for_correct_guesses
     card = Card.new("What is the capital of Alaska?", 'Juneau')
-    guess = Guess.new("Jeneau", card)
+    guess = Guess.new("Juneau", card)
 
-    assert_equal '', guess.card
+    assert_equal card, guess.card
     assert_equal true, guess.correct?
     assert_equal 'Correct!', guess.feedback
+  end
+
+  def test_guess_returns_correct_feedback_and_boolean_for_incorrect_guesses
+    card = Card.new("Which planet is closest to the sun?", "Mercury")
+    guess = Guess.new("Saturn", card)
+
+    assert_equal card, guess.card
+    assert_equal false, guess.correct?
+    assert_equal "Incorrect.", guess.feedback
   end
 end
