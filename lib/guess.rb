a04f1fe46c78fad6729a50201cd14d0e65b964f7
Tim Tyrrell <timomitchel23@gmail.com>
2017-09-23 08:44:45 -0600

    it 3

--- lib/guess.rb
+++ lib/guess.rb
@@ -8,11 +8,7 @@ class Guess
   end
 
   def correct?
-    if @guess == @card.answer
-      true
-    else
-      false
-    end
+    @guess == @card.answer
   end
 
   def feedback
--- test/card_test.rb
+++ test/card_test.rb
@@ -2,6 +2,7 @@ require 'pry'
 require 'minitest/autorun'
 require "./lib/guess"
 require './lib/card'
+require './lib/deck'
 
 class CardTest < Minitest::Test
 
@@ -34,4 +35,15 @@ class CardTest < Minitest::Test
     assert_equal false, guess.correct?
     assert_equal "Incorrect.", guess.feedback
   end
+
+  def test_cards_can_get_stored_in_a_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
+    deck = Deck.new([card_1, card_2, card_3])
+
+    assert_instance_of Array, deck.cards
+    assert_equal 3, deck.count
+  end
+
 end
